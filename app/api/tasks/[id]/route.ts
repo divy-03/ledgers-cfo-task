9a4694542b263771cf6df928cf4e1281e23de894
divy-03 <pathakdivy03@gmail.com>
2026-03-24 02:48:26 +0530

    fixed params error and prisma

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -4,7 +4,7 @@ import { updateTaskStatusSchema } from "@/lib/zod";
 
 export async function PATCH(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
